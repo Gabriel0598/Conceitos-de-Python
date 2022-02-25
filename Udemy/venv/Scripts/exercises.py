10f3692e7afd5994d248c9138b6ea5b50e0e0e75
Gabriel A Ferreira <Gabriel.A.Ferreira@br.ey.com>
2022-02-25 15:14:16 -0300

    C2 - Basic exercises

---
+++ Udemy/venv/Scripts/exercises.py
@@ -0,0 +1,26 @@
+# Concatening exercise
+var_1 = 3.14159
+print(type(var_1))
+print(str(var_1) + " is a float.")
+name = "Gabriel"
+print("Hello, i'm " + name + " it's nice to meet you!")
+
+print("*******\n ***** \n  ***  \n   *   ")
+
+# Just do it exercice
+just = "Just do it!"
+print(just[10:])
+print(just[5:7])
+print(just[8:10])
+print(just[0:4])
+print("Don't " + just[5:11])
+
+# Input exercise
+name = input("What is your name? ")
+quest = input("What is your quest? ")
+color = input("What is your favorite color? ")
+
+print("So your name is " + name + ", your quest is " + quest + ", and your favorite color is " + color + ".")
+
+int_1 = int(input("Enter with a number: "))
+print(int_1 + 10)
