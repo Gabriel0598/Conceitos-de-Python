38331ab2d3793347e5d7c9566a8c39b5e409bc92
Gabriel A Ferreira <Gabriel.A.Ferreira@br.ey.com>
2022-03-04 00:16:08 -0300

    C4 - Exercises complement - Section 4

--- Udemy/venv/Scripts/exercises.py
+++ Udemy/venv/Scripts/exercises.py
@@ -14,7 +14,7 @@ print(just[5:7])
 print(just[8:10])
 print(just[0:4])
 print("Don't " + just[5:11])
-
+'''
 # Input exercise
 name = input("What is your name? ")
 quest = input("What is your quest? ")
@@ -24,3 +24,42 @@ print("So your name is " + name + ", your quest is " + quest + ", and your favor
 
 int_1 = int(input("Enter with a number: "))
 print(int_1 + 10)
+'''
+def hello_world_printer():
+    print("Hello World")
+
+hello_world_printer()
+
+name
+def name_printer(user_name):
+    print(user_name)
+'''
+name = input("Please enter your name: ")
+name_printer(name)
+
+length = int(input("Enter an integer that represents length in feet: "))
+width = int(input("Enter an integer that represents width in feet: "))
+height = int(input("Enter an integer that represents height in feet: "))
+
+def prism_vol(l, w, h):
+    return l * w * h
+
+print("The volume of the rectangular prism is " + str(prism_vol(length, width, height)) + " cubic feet.")
+###
+
+###
+celsius = int(input("Enter an temperature in celsius degree: "))
+def fahrenheit(cel):
+    return round((1.8 * cel + 32), 1)
+
+print("The Fahrenheit equivalent of " + str(celsius) + " degrees Celsius is " + str(fahrenheit(celsius)) + ".")
+'''
+from random import randint
+gallons = (randint(10, 25))
+miles = (randint(200, 400))
+
+# formula = miles per gallon (MPG) = miles driven/gallons used
+MPG = miles // gallons
+print("The car can travel " + str(MPG) + " Miles per gallon.")
+print("The car's fuel tank can hold " + str(gallons) + " gallons.")
+print("The car can travel " + str(miles) + " miles on a full tank.")
