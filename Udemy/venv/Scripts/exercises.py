cfc21dab669bc5cab5f548eefd011cd6862fe4fe
Gabriel A Ferreira <Gabriel.A.Ferreira@br.ey.com>
2022-03-06 15:04:03 -0300

    C6 - Exercise grade

--- Udemy/venv/Scripts/exercises.py
+++ Udemy/venv/Scripts/exercises.py
@@ -63,3 +63,21 @@ MPG = miles // gallons
 print("The car can travel " + str(MPG) + " Miles per gallon.")
 print("The car's fuel tank can hold " + str(gallons) + " gallons.")
 print("The car can travel " + str(miles) + " miles on a full tank.")
+
+# Grade determine
+score = int(input("Please enter the student's score. "))
+
+if score >= 90:
+    print("This student's score of " + str(score) + " is an A.")
+else:
+    if score >= 80:
+        print("This student's score of " + str(score) + " is a B.")
+    else:
+        if score >= 70:
+            print("This student's score of " + str(score) + " is a C.")
+        else:
+            if score >= 60:
+                print("This student's score of " + str(score) + " is a D.")
+            else:
+                print("This student's score of " + str(score) + " is a F.")
+
